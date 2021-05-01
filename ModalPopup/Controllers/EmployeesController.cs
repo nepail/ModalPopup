c218dfdb714389bb1b959ab12cfc92c5fbc82c26
Lin Nepail <kls18z@hotmail.com>
2021-05-01 22:42:17 +0800

    加入專案檔案。

---
+++ ModalPopup/Controllers/EmployeesController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ModalPopup.Data;
+using ModalPopup.Models;
+
+namespace ModalPopup.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public EmployeesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var listofEmployees = _context.Employees.ToList();
+            return View(listofEmployees);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            Employee emp = new Employee();
+            return PartialView("_EmployeeModelPartial", emp);
+        }
+        [HttpPost]
+        public IActionResult Create(Employee emp)
+        {
+            _context.Employees.Add(emp);
+            _context.SaveChanges();
+            return PartialView("_EmployeeModelPartial", emp);
+        }
+    }
+}
---
+++ ModalPopup/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ModalPopup.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace ModalPopup.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+             : base(options)
+        {
+
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
